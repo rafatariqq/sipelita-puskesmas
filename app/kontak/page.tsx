0a3c1c1fb9b1c29a1d21f0e6c420f227777ba9db
rafatariqq <satriapota201@gmail.com>
2026-09-25 12:15:15 +0700

    Update page.tsx

--- app/kontak/page.tsx
+++ app/kontak/page.tsx
@@ -9,8 +9,9 @@ export default function Kontak() {
   );
   const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${whatsappMessage}`;
 
-  // Link Google Maps berdasarkan koordinat Lat Long
-  const googleMapsUrl = "https://www.google.com/maps?q=-2.3112276,119.1766989";
+  // Tautan Google Maps ke Puskesmas Satelit Tarailu
+  const googleMapsUrl =
+    "https://www.google.com/maps/place/Puskesmas+Satelit+Tarailu/@-2.3111475,119.1754624,18z/data=!4m23!1m16!4m15!1m6!1m2!1s0x2d92d833d4d70f27:0xec8b6ecb8c7bb406!2sJl.+Poros+Mamuju+-+Topoyo,+Sulawesi+Barat!2m2!1d119.1621854!2d-2.4195697!1m6!1m2!1s0x2d9290f914f05173:0xb0b84e39a98a507!2sTarailu,+Kec.+Sampaga,+Kabupaten+Mamuju,+Sulawesi+Barat!2m2!1d119.179765!2d-2.3173016!3e0!3m5!1s0x2d9291e7019333a9:0x7ba3c2a8525060fc!8m2!3d-2.3112276!4d119.1766989!16s%2Fg%2F11ghsjk6wk";
 
   return (
     <main>
@@ -37,7 +38,7 @@ export default function Kontak() {
                   textDecoration: "underline",
                 }}
               >
-                Jl. Contoh No. 10, Indonesia
+                Puskesmas Satelit Tarailu, Kec. Sampaga, Kab. Mamuju, Sulawesi Barat
               </a>
             </p>
           </div>
